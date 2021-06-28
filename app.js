c6b44fcc5240fe6ead97e9b4ecb432d531045a23
asgharali9211 <86408007+asgharali9211@users.noreply.github.com>
2021-06-28 16:55:21 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,173 @@
+// ch # 1               ALERT
+//q1,q2
+//alert("Error! Please enter a valid password");
+//q3
+// var str = "Welocome to Js Land \n Happy coding!";
+// alert(str);
+//Q4
+// alert("Welcome to JS Land");
+// var a= "happy coding! \n prevent this page for creating additional dialogs. "
+// alert(a);
+// q5
+// alert("hello i can run JS through my browser's console");
+// console.log("hello i can run JS through my browser's console");
+
+// Q6,Q7
+
+//                  ch # 02   
+
+// // Q1
+// var username;
+// //Q2
+// var myName = "syed asghar ali"; 
+// document.write(myName);
+// //Q3
+// var message = "hello world";
+// alert(message);
+
+// //Q4
+// var name = "jhone doe";
+// var age= "15 year old"
+// var course ="certified mobile & app development"
+// alert(name);
+// alert(age);
+// alert(course);
+
+//q5
+
+// var p = "PIZZA \n PIZZ \n PIZ \n PI \n p"
+// alert(p); 
+
+// q6
+
+// var email ="syedasgharali1996@gmail.com"
+// alert("my email address is " + email);
+
+
+// Q7
+
+// var book = "A SMARTER WAY TO LEARN JAVASCRIPT"
+// alert("I am trying to learn from the book " + book);
+
+// Q8
+
+// document.write("yah! i can write html content through javascript");
+
+// q9
+
+// document.write("-----------$$$$----------");
+// alert("-----------$$$$----------");
+
+            //    ch #3
+// q1
+// var age=24;
+// alert("i am "+ age +" year old");
+
+
+// q2
+// var n;
+// var b=prompt("n");
+// alert("you have visited this website " + b + " times");
+
+
+// q3
+// var birthYear = 1996;
+// document.write("my birth year is " + birthYear +"<br/> data type of my decleared variable is number");
+
+
+// q4
+
+// var visters_name= "<b>john doe</b>";
+// var productTittle=" <b>t shirts</b>"
+// var quantity="<b>5</b>";
+// document.write(visters_name+ " ordered " + quantity + productTittle +" on xyz clothing cloth");
+
+
+                
+            //ch 04
+//q1
+// var $name= "asghar";
+// var age ="24";
+// var city="karachi"
+// document.write( $name +age+ city);
+
+// q2
+
+// var $name;
+// var firstName;
+// var name987;
+// var first_name;
+// var ali;
+
+
+// var 086name;
+// var asghar ali;
+// var 77566;
+// var P2_a%88$;
+// var asghar-ali;
+  
+
+// q3
+
+// document.write("<b>Rules for naming JS variables</b>");
+// document.write(" </br> Variable names can only contain number , $ and _ . For example $my_1stVariable");
+// document.write("</br> Variables must begin with a letter, $ and _ .For example $name, _name or name");
+// document.write("</br> Variable names are case sensitive");
+// document.write("</br> Variable names should not be JS keywords")
+
+            //ch# 05
+//q1
+// var a=3;
+// var b=5;
+// c= a+b;
+// document.write("sum of "+ a+ " and "+ b+ " is "+ c);
+
+// //q2
+
+// var a=3;
+// var b=5;
+// d= a-b;
+// document.write("</br> subtraction of "+ a+ " and "+ b+ " is "+ d);
+
+// e= a*b;
+// document.write("</br>  multiplication of "+ a+ " and "+ b+ " is "+ e);
+
+
+// f= a/b;
+// document.write("</br>  division of "+ a+ " and "+ b+ " is "+ f);
+
+
+//q3
+
+// var a;
+// document.write("Value after variable declaration is "+a);
+// a=5;
+// document.write("</br>Initial value "+a);
+// a=a+1;
+// document.write("</br> Value after increment is "+a);
+// a=a+7;
+// document.write("</br>Value after increment is "+a);
+// a = --a;
+// document.write("</br> Value after decrement is: "+a)
+// a = a%3;
+// document.write("</br>The remainder is : "+ a )
+
+//q4
+
+// var ticket = 600;
+// output=600*5;
+// document.write("total cost of buy 5 ticket to a movie is " + output);
+
+
+//q5
+
+// var a=4;
+// document.write("table of 4<br/>");
+// for(b=1; b<=10; b++){
+//     c=a*b
+
+// document.write(a+ " * "+b+" = "+c+ "<br/>")
+// }
+
+//q6
+
